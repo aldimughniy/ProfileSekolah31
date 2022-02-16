a2628ea787d31046e2e3a92ded55396320f0f20a
anggun123456 <332anggunseptiana@gmail.com>
2022-02-16 14:05:06 +0700

    lanjutan ketiga

--- app/src/main/java/com/example/profilesekolah/galeriActivity.java
+++ app/src/main/java/com/example/profilesekolah/galeriActivity.java
@@ -3,12 +3,28 @@ package com.example.profilesekolah;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
 
 public class galeriActivity extends AppCompatActivity {
-
+    ViewFlipper viewFlipper;
+     Animation fadein,fadeout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeri);
+
+        viewFlipper =(ViewFlipper) findViewById(R.id.ViewFlipper);
+        fadein = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        fadeout=AnimationUtils.loadAnimation(this,R.anim.fade_out);
+
+        viewFlipper.setInAnimation(fadein);
+        viewFlipper.setOutAnimation(fadein);
+
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.startFlipping();
     }
+
 }
